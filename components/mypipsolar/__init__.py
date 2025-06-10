ce654966a64ffee5d846192c5e24ea6754f9cc59
GamesterUnknown <160248229+GamesterUnknown@users.noreply.github.com>
2025-06-11 02:28:25 +0300

    Update __init__.py

--- components/mypipsolar/__init__.py
+++ components/mypipsolar/__init__.py
@@ -1,22 +1,26 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import uart
+from esphome.components import uart, pipsolar
 from esphome.const import CONF_ID
 
-# Імпортуємо неймспейс батьківського компонента
-parent_ns = cg.esphome_ns.namespace('pipsolar')
-PipSolar = parent_ns.class_('PipSolar')  # Так оголошуємо клас
+mypipsolar_ns = cg.esphome_ns.namespace("mypipsolar")
+MyPipSolar = mypipsolar_ns.class_("MyPipSolar", pipsolar.PipsolarComponent)
 
-# Створюємо свій неймспейс
-mypipsolar_ns = cg.esphome_ns.namespace('mypipsolar')
+# Візьмемо схему зсередини cv.All батьківського CONFIG_SCHEMA:
+PARENT_SCHEMA = cv.Schema({cv.GenerateID(): cv.declare_id(pipsolar.PipsolarComponent)})\
+    .extend(cv.polling_component_schema("1s"))\
+    .extend(uart.UART_DEVICE_SCHEMA)
 
-# Визначаємо клас-нащадок
-MyPipSolar = mypipsolar_ns.class_('MyPipSolar', PipSolar)
-
-# Схема конфігурації
-CONFIG_SCHEMA = parent_ns.CONFIG_SCHEMA  # або інша схема, якщо є
+# Тепер розширимо її додатковими параметрами для mypipsolar
+CONFIG_SCHEMA = cv.All(
+    PARENT_SCHEMA.extend({
+        # тут додаємо свої додаткові параметри, наприклад:
+        # cv.Optional("custom_param"): cv.string,
+    })
+)
 
 def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     yield cg.register_component(var, config)
     yield uart.register_uart_device(var, config)
+    # Тут додай свій додатковий код ініціалізації, якщо треба
