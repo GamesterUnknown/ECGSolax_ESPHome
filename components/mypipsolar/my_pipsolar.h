096be1407a24a67e7cdd70286161c8bf9b400c22
GamesterUnknown <160248229+GamesterUnknown@users.noreply.github.com>
2025-06-11 02:06:34 +0300

    Update my_pipsolar.h

--- components/mypipsolar/my_pipsolar.h
+++ components/mypipsolar/my_pipsolar.h
@@ -3,20 +3,16 @@
 #include "pipsolar.h"
 #include "esphome/core/component.h"
 #include "esphome/components/time/real_time_clock.h"
-#include "esphome/core/yaml.h"  // Для esphome::yaml::Schema
 
 namespace esphome {
 namespace mypipsolar {
 
-class MyPipSolar : public PipSolar {
+class MyPipSolar : public pipsolar::PipSolar {
  public:
   void setup() override;
   void send_set_datetime();
-  
-  static const esphome::yaml::Schema &get_config_schema();
 };
 
-extern const esphome::yaml::Schema CONFIG_SCHEMA;
-
-}  // namespace pipsolar
+}  // namespace mypipsolar
 }  // namespace esphome
+
