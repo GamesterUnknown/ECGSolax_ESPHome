4efe66edf7553d045cfcf6d3ae61d766406c8597
GamesterUnknown <160248229+GamesterUnknown@users.noreply.github.com>
2025-06-11 02:20:33 +0300

    Update __init__.py

--- components/mypipsolar/__init__.py
+++ components/mypipsolar/__init__.py
@@ -2,15 +2,19 @@ import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import uart
 from esphome.const import CONF_ID
-from esphome.components import pipsolar as parent
 
-DEPENDENCIES = ['uart']
-AUTO_LOAD = ['sensor', 'text_sensor', 'binary_sensor', 'switch', 'output', 'select']
+# Імпортуємо неймспейс батьківського компонента
+parent_ns = cg.esphome_ns.namespace('pipsolar')
+PipSolar = parent_ns.class_('PipSolar')  # Так оголошуємо клас
 
-mypipsolar_ns = cg.esphome_ns.namespace('pipsolar')
-MyPipSolar = mypipsolar_ns.class_('MyPipSolar', parent.PipSolar)
+# Створюємо свій неймспейс
+mypipsolar_ns = cg.esphome_ns.namespace('mypipsolar')
 
-CONFIG_SCHEMA = parent.CONFIG_SCHEMA  # просто наслідуємо без розширень
+# Визначаємо клас-нащадок
+MyPipSolar = mypipsolar_ns.class_('MyPipSolar', PipSolar)
+
+# Схема конфігурації
+CONFIG_SCHEMA = parent_ns.CONFIG_SCHEMA  # або інша схема, якщо є
 
 def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
