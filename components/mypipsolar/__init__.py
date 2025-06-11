2a4c0fd5f69b6cbd43f1a582fcf1763d174f73d9
GamesterUnknown <160248229+GamesterUnknown@users.noreply.github.com>
2025-06-11 17:15:44 +0300

    Update __init__.py

--- components/mypipsolar/__init__.py
+++ components/mypipsolar/__init__.py
@@ -1,29 +1,33 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import uart, time as time_component, pipsolar
+from esphome.components import uart, time as time_component
 from esphome.const import CONF_ID
 
+DEPENDENCIES = ["uart", "pipsolar"]
+CODEOWNERS = ["@GamesterUnknown"]
+AUTO_LOAD = ["binary_sensor", "text_sensor", "sensor", "switch", "output", "select"]
+MULTI_CONF = True
+
+CONF_MY_PIPSOLAR_ID = "my_pipsolar_id"
+
 mypipsolar_ns = cg.esphome_ns.namespace("mypipsolar")
-MyPipSolar = mypipsolar_ns.class_("MyPipSolar", cg.Component)
+PipsolarBase = cg.global_ns.namespace("pipsolar").class_("Pipsolar", cg.Component)
+MyPipSolar = mypipsolar_ns.class_("MyPipSolar", PipsolarBase)
 
-CONF_TIME_ID = "time_id"
+CONFIG_SCHEMA = cv.Schema(
+    {
+        cv.GenerateID(): cv.declare_id(MyPipSolar),
+        cv.Optional("time_id"): cv.use_id(time_component.RealTimeClock),
+    }
+).extend(cv.polling_component_schema("1s")).extend(uart.UART_DEVICE_SCHEMA)
 
-# Беремо базову схему з pipsolar та розширюємо її додатковим параметром time_id
-PARENT_SCHEMA = cv.Schema({cv.GenerateID(): cv.declare_id(pipsolar.PipsolarComponent)})\
-    .extend(cv.polling_component_schema("1s"))\
-    .extend(uart.UART_DEVICE_SCHEMA)
-
-CONFIG_SCHEMA = cv.All(
-    PARENT_SCHEMA.extend({
-        cv.Optional(CONF_TIME_ID): cv.use_id(time_component.RealTimeClock),
-    })
-)
 
 def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     yield cg.register_component(var, config)
     yield uart.register_uart_device(var, config)
 
-    if CONF_TIME_ID in config:
-        time_var = yield cg.get_variable(config[CONF_TIME_ID])
-        cg.add(var.set_time_component(time_var))
+    if "time_id" in config:
+        time_ = yield cg.get_variable(config["time_id"])
+        cg.add(var.set_time_component(time_))
+
