fc744341791d4b30255a4bd3c11034dfaaac86c8
GamesterUnknown <160248229+GamesterUnknown@users.noreply.github.com>
2025-06-11 22:12:55 +0300

    Update __init__.py

--- components/mypipsolar/__init__.py
+++ components/mypipsolar/__init__.py
@@ -1,6 +1,6 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import uart, time as time_component
+from esphome.components import pipsolar, uart, time as time_component
 from esphome.const import CONF_ID
 
 DEPENDENCIES = ["uart", "pipsolar"]
