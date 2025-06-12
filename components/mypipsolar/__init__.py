8991d4b22758ad7cb7b842cbde520e2d7ba2f25f
GamesterUnknown <160248229+GamesterUnknown@users.noreply.github.com>
2025-06-12 12:24:15 +0300

    Update __init__.py

--- components/mypipsolar/__init__.py
+++ components/mypipsolar/__init__.py
@@ -3,11 +3,11 @@ import esphome.config_validation as cv
 from esphome.components import pipsolar, uart, time as time_component
 from esphome.const import CONF_ID
 from esphome.components.pipsolar import PipsolarComponent
-
-DEPENDENCIES = ["uart"]
+from esphome.components.pipsolar import PIPSOLAR_COMPONENT_SCHEMA
+#DEPENDENCIES = ["uart"]
 CODEOWNERS = ["@GamesterUnknown"]
 #AUTO_LOAD = ["binary_sensor", "text_sensor", "sensor", "switch", "output", "select"]
-MULTI_CONF = True
+#MULTI_CONF = True
 
 CONF_MY_PIPSOLAR_ID = "my_pipsolar_id"
 CONF_TIME_ID = "time_id"
@@ -16,13 +16,16 @@ mypipsolar_ns = cg.esphome_ns.namespace("mypipsolar")
 # PipsolarComponent = cg.global_ns.namespace("pipsolar").class_("Pipsolar", cg.Component)
 MyPipSolar = mypipsolar_ns.class_("MyPipSolar", PipsolarComponent)
 
-CONFIG_SCHEMA = cv.Schema(
-    {
-        cv.GenerateID(): cv.declare_id(MyPipSolar),
-        cv.Optional("time_id"): cv.use_id(time_component.RealTimeClock),
-    }
-).extend(cv.polling_component_schema("1s")).extend(uart.UART_DEVICE_SCHEMA)
+#CONFIG_SCHEMA = cv.Schema(
+#    {
+#        cv.GenerateID(): cv.declare_id(MyPipSolar),
+#        cv.Optional("time_id"): cv.use_id(time_component.RealTimeClock),
+#    }
+#).extend(cv.polling_component_schema("1s")).extend(uart.UART_DEVICE_SCHEMA)
 
+CONFIG_SCHEMA = pipsolar.PIPSOLAR_COMPONENT_SCHEMA.extend({
+    cv.Optional("time_id"): cv.use_id(time_component.RealTimeClock),
+})
 
 def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
